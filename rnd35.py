0e7abbebf241098b5d146674a7e4ea2182edeab5
Hyun949494 <kimchungh1994@gmail.com>
2025-08-26 17:10:40 +0900

    Update rnd35.py

--- rnd35.py
+++ rnd35.py
@@ -4,8 +4,8 @@ TEAM_CONFIG = {
     "I": "metformin",
     "C": "placebo",
     "O": "glycemic control",
-    "email": "rnd35@company.com",
-    "api_key": "",
-    "product": "Metformin",
+    "email": "yuchan970101@gmail.com",
+    "api_key": "69e34d45728c0306f62e7e1713d57c8ad808",
+    "product": "",
     "gemini_api_key": ""
 }
